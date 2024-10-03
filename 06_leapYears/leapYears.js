d64d34727c40bc5c0b8edf38496459edcf39ef3d
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-03 18:01:22 +0530

    Update solution for exercise_06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(inpYear) {
+    if (inpYear % 100 == 0)
+    {
+        if ( inpYear % 400 == 0)
+        {
+            return true;
+        }
+    } else if (inpYear % 4 == 0)
+    {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
