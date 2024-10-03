de27abc38608d5e7490e427fa4d725d984ac9013
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-03 18:16:00 +0530

    Update solution for exercise_07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inpFahrenheit) {
+let tempInCelsius = (inpFahrenheit - 32) * (5/9.0);
+return parseFloat(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inpCelsius) {
+  let tempInFahrenheit = (inpCelsius * (9 / 5.0)) + 32.0;
+  return parseFloat(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
