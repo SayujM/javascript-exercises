2057b78dce4728fcd2089a42806260616bd8138d
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-21 19:33:19 +0530

    Update solution for exercise 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,44 @@
-const add = function() {
-	
+const add = function(x=0, y=0) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x=0, y=0) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let sum = 0;
+  for ( let i = 0, length = list.length; i < length; i++)
+  {
+    sum += list[i];
+  }
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let multiple = 1;
+  for ( let i = 0, length = list.length; i < length; i++)
+    {
+      multiple *= list[i];
+    }
+    return multiple;
 };
 
-const power = function() {
-	
+const power = function(x=1, y=1) {
+	let power = 1;
+  for (let i = 0; i < y; i++)
+  {
+    power *= x  
+  }
+  return power;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if ( n <= 1)
+  {
+    return 1;
+  }
+  return n * factorial(n-1);
 	
 };
 
