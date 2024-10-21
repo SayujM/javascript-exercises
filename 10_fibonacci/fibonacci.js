6059bd81c160e2309d844e04492b2e3d65b5e40d
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-21 20:02:18 +0530

    Update solution for exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibLength = parseInt(n);
+    if ( fibLength < 0)
+    {
+        return "OOPS";
+    } else if(fibLength === 0)
+    {
+        return 0;
+    } else if (fibLength <= 2 )
+    {
+        return 1;
+    } else
+    {
+        return fibonacci(fibLength - 1) + fibonacci(fibLength - 2);
+    }
 
 };
 
