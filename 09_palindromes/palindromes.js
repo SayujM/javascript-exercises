f72cef55366d86534041321203af54a7ed1dbacd
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-21 19:51:40 +0530

    Update solution for exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let string1 = '';
+    let pattern = /[a-zA-Z0-9]/;
+    for (let i = 0, length = text.length; i < length; i++)
+    {
+        if (pattern.test(text[i]))
+        {
+            string1 += text[i]
+        }
+    }
+    string1 = string1.toLowerCase();
+    let string2 = string1.split('').reverse().join('');
+    if (string1 === string2)
+    {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
