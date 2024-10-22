cf72167c970dabcf7beff6c544f0afd124b3919b
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-22 19:53:31 +0530

    Update my solution for exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let outArray = array.map((element) => element["title"]);
+    return outArray;
 };
 
 // Do not edit below this line
