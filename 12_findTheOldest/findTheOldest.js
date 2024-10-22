5d19c7c17e0b5f6212cfe57e6c5cc83cb1f22559
SayujM <70249352+SayujM@users.noreply.github.com>
2024-10-22 20:16:29 +0530

    Update my solution for exercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleAge = people.map(person => ({
+        name: person["name"],
+        age: (person["yearOfDeath"] ? person["yearOfDeath"] : new Date().getFullYear()) - person["yearOfBirth"]
+    }));
 
+    peopleAge = peopleAge.reduce((person1, person2) => {
+        return (person1.age > person2.age) ? person1 : person2;
+    });
+
+    return peopleAge;
 };
 
 // Do not edit below this line
